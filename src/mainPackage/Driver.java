0542b07bf6a2f8db52fdaef265a94760f0abf1b9
BCSBruh <87160862+BCSBruh@users.noreply.github.com>
2023-04-16 05:03:21 -0400

    Created the Screen class

--- src/mainPackage/Driver.java
+++ src/mainPackage/Driver.java
@@ -4,13 +4,20 @@ import java.applet.Applet;
 
 public class Driver extends Applet {
 	public Camera camera;
+	public Screen screen;
 
 	public Driver() {
 		camera = new Camera(4.5, 4.5, 1, 0, 0., -.66);
 		addKeyListener(camera);
+
+		//Initialize textures
+		
+		//construct screen
+		screen = new Screen(map, mapWidth, mapHeight, textures, 640, 480);
 	}
 
 	public void run() {
+		//Need to add screen.update(camera, pixels)
 		//Need to add camera.update(map) within the while (delta >= 1) loop
 	}
 
---
+++ src/mainPackage/Screen.java
@@ -0,0 +1,143 @@
+package mainPackage;
+
+import java.util.ArrayList;
+import java.awt.Color;
+
+public class Screen {
+    public int[][] map;
+
+    public int mapWidth, mapHeight, width, height;
+    public ArrayList textures;
+
+    public Screen(int[][] m, ArrayList tex, int w, int h) {
+        map = m;
+        textures = tex;
+        width = w;
+        height = h;
+    }
+
+    public int[] update (Camera camera, int[] pixels) {
+        //First reset the pixels to just a floor and a roof
+        //Create a floor
+        for (int i = 0; i < pixels.length/2; i++) {
+            if (pixels[i] != Color.DARK_GRAY.getRGB())
+                pixels[i] = Color.DARK_GRAY.getRGB();
+        }
+
+        //Create a ceiling
+        for (int i = pixels.length/2; i < pixels.length; i++) {
+            if (pixels[i] != Color.GRAY.getRGB())
+                pixels[i] = Color.GRAY.getRGB();
+        }
+
+        for (int x = 0; x < width; x = x + 1) {
+            double cameraX = 2*x / (double)(width) - 1;
+            double rayDirX = camera.xDir + camera.xPlane * cameraX;
+            double rayDirY = camera.yDir + camera.yPlane * cameraX;
+
+            double sideDistX, sideDistY; //Length of ray from current position
+
+            //Position on the map
+            int mapX = (int)camera.xPos;
+            int mapY = (int)camera.yPos;
+
+            //Length of ray form current position to next side
+            double deltaDistX = Math.sqrt(1 + (rayDirY*rayDirY) / (rayDirX*rayDirX));
+            double deltaDistY = Math.sqrt(1 + (rayDirX*rayDirX) / (rayDirY*rayDirY));
+            double wallDist;    //Wall distance from player that ray hits
+
+            //Find the direction to go in x and y plane
+            int stepX, stepY;
+            boolean wall = false;
+            int side = 0; //Did we hit the corner of a wall or face
+
+            //Figure out which direction to step and how the initial distance to a side of the wall
+            if (rayDirX < 0) {
+                stepX = -1;
+                sideDistX = (camera.xPos - mapX) * deltaDistX;
+            }else {
+                stepX = 1;
+                sideDistX = (mapX + 1.0 - camera.xPos) * deltaDistX;
+            }
+
+            if (rayDirY < 0) {
+                stepY = -1;
+                sideDistY = (camera.yPos - mapY) * deltaDistY;
+            }else {
+                stepY = 1;
+                sideDistY = (mapY + 1.0 - camera.yPos) * deltaDistY;
+            }
+
+            //Check to see if ray has collided with a wall
+            while (!wall) {
+                //Go to next square
+                if (sideDistX < sideDistY) {
+                    sideDistX += deltaDistX;
+                    mapX += stepX;
+                    side = 0;
+                }else{
+                    sideDistY += deltaDistY;
+                    mapY += stepY;
+                    side = 1;
+                }
+
+                //Check if that next square is a wall
+                if(map[mapX][mapY] > 0)
+                    wall = true;
+            }
+
+            //Get distance to the point of impact
+            if (side == 0)
+                wallDist = Math.abs((mapX - camera.xPos + (1 - stepX) / 2) / rayDirX);
+            else
+                wallDist = Math.abs((mapY - camera.yPos + (1 - stepY) / 2) / rayDirY);
+
+            //Now find the height of the wall based on how far away it is from the camera
+            int lineHeight = height;
+            if (wallDist > 0)
+                lineHeight = Math.abs((int)(height / wallDist));
+            else
+                lineHeight = height;
+
+            //Find the lowest and highest pixel to fill current vertical strip
+            int drawStart = -lineHeight/2 + height/2;
+            if (drawStart < 0)
+                drawStart = 0;
+
+            int drawEnd = lineHeight/2 + height/2;
+            if (drawEnd >= height)
+                drawEnd = height - 1;
+
+            //Put textures onto that wall
+            int texNum = map[mapX][mapY] - 1;
+            double wallX;   //Exact position where wall was hit
+
+            if (side == 1)
+                wallX = (camera.xPos + ((mapY - camera.yPos + (1 - stepY) / 2) / rayDirY) * rayDirX); //Y-axis wall
+            else
+                wallX = (camera.yPos + ((mapX - camera.xPos + (1 - stepX) / 2) / rayDirX) * rayDirY); //X-axis wall
+
+            wallX -= Math.floor(wallX);
+
+            //x coordinate on the texture
+            int texX = (int)(wallX * (textures.get(texNum).SIZE));
+            if ((side == 0 && rayDirX > 0) || (side == 1 && rayDirY < 0))
+                texX = textures.get(texNum).SIZE - texX - 1;
+
+            for (int y = drawStart; y < drawEnd; y++) {
+                int texY = (((y*2 - height + lineHeight) << 6) / lineHeight) /2;
+                int color;
+
+                if (side == 0)
+                    color = textures.get(texNum).pixels[texX + (texY * textures.get(texNum).SIZE)];
+                else
+                    color = (textures.get(texNum).pixels[texX + (texY * textures.get(texNum).SIZE)] >> 1) & 8355711;
+
+                pixels[x + y*(width)] = color;
+            }
+
+        }
+
+        return pixels;
+    }
+}
