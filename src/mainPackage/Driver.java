cf050a2257383272239f0e1037ffba6fba05a52d
BCSBruh <jeromelarson26@gmail.com>
2023-04-19 13:29:42 -0400

    Created Background

--- src/mainPackage/Driver.java
+++ src/mainPackage/Driver.java
@@ -4,9 +4,12 @@ import com.sun.j3d.utils.applet.MainFrame;
 import com.sun.j3d.utils.universe.SimpleUniverse;
 
 import java.applet.Applet;
-import javax.media.j3d.BranchGroup;
-import javax.media.j3d.Canvas3D;
+import javax.imageio.ImageIO;
+import javax.media.j3d.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Driver extends Applet {
 	public void init() {
@@ -16,25 +19,40 @@ public class Driver extends Applet {
 		setLayout(new BorderLayout());
 		add(cv, BorderLayout.CENTER);
 
-		BranchGroup pipes = createPipe();
-		BranchGroup back = createBackground();
-		BranchGroup player = createPlayer();
-		pipes.compile();
-		player.compile();
+//		BranchGroup pipes = createPipe();
+		BranchGroup back = null;
+		try {
+			back = createBackground();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+//		BranchGroup player = createPlayer();
+//		pipes.compile();
+//		player.compile();
 
 		SimpleUniverse su = new SimpleUniverse(cv);
 		su.getViewingPlatform().setNominalViewingTransform();
-		su.addBranchGraph(pipes);
+//		su.addBranchGraph(pipes);
 		su.addBranchGraph(back);
-		su.addBranchGraph(player);
+//		su.addBranchGraph(player);
 	}
 
 	private BranchGroup createPipe() {
 		return null;
 	}
 
-	private BranchGroup createBackground() {
-		return null;
+	private BranchGroup createBackground() throws IOException {
+		BranchGroup backgroundGroup = new BranchGroup();
+
+		BufferedImage backImage = ImageIO.read(new File("res/back.png"));
+		ImageComponent2D imageComp = new ImageComponent2D(ImageComponent.FORMAT_RGB, backImage);
+
+		Background b = new Background(imageComp);
+		b.setApplicationBounds(new BoundingSphere());
+		backgroundGroup.addChild(b);
+
+		return backgroundGroup;
+
 	}
 
 	private BranchGroup createPlayer() {
